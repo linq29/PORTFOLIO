1fe3d7b054ee1400d05436c06607b3aab8a3ed39
LinQ <linqiujing77@gmail.com>
2026-03-15 02:49:23 +0900

    add: language toggle

---
+++ js/translations/index.js
@@ -0,0 +1,10 @@
+import { en } from "./en.js";
+import { ja } from "./ja.js";
+
+// Register new locale modules here.
+export const translations = {
+  ja,
+  en
+};
+
+export const defaultLanguage = "ja";
